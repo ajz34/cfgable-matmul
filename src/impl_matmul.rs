df4ff6fe9eebdc0c2abc1cf2dcb36917e7c3cb73
ajz34 <ajz34@outlook.com>
2025-12-15 19:32:39 +0800

    pack trans b

--- src/impl_matmul.rs
+++ src/impl_matmul.rs
@@ -74,6 +74,42 @@ where
     }
 
     #[inline]
+    pub fn pack_b_no_trans(dst: &mut [[TySimd<T, LANE>; NR_LANE]], src: &[T], kc: usize, nr: usize, ldb: usize) {
+        if nr == NR_LANE * LANE {
+            for p in 0..kc {
+                let b_ptr = unsafe { src.as_ptr().add(p * ldb) };
+                for j_lane in 0..NR_LANE {
+                    dst[p][j_lane] = unsafe { TySimd::loadu_ptr(b_ptr.add(j_lane * LANE)) };
+                }
+            }
+        } else {
+            // avoid out-of-bound access
+            for p in 0..kc {
+                for jj in 0..nr {
+                    let j_lane = jj / LANE;
+                    let j_offset = jj % LANE;
+                    dst[p][j_lane][j_offset] = src[p * ldb + jj].clone();
+                }
+            }
+        }
+    }
+
+    #[inline]
+    pub fn pack_b_trans(dst: &mut [[TySimd<T, LANE>; NR_LANE]], src: &[T], kc: usize, nr: usize, ldb: usize, buf: &mut [T]) {
+        // first copy to temporary buffer without transpose
+        for j in 0..nr {
+            buf[j * KC..j * KC + kc].clone_from_slice(&src[j * ldb..j * ldb + kc]);
+        }
+
+        let dst_slc = unsafe { core::slice::from_raw_parts_mut(dst.as_mut_ptr() as *mut T, KC * NR_LANE * LANE) };
+        for p in 0..kc {
+            for j in 0..NR_LANE * LANE {
+                dst_slc[p * NR_LANE * LANE + j] = buf[j * KC + p].clone();
+            }
+        }
+    }
+
+    #[inline]
     pub fn matmul_loop_2nd_nr_pack_b(
         c: &mut [T],           // MC x NC (ldc), DRAM
         a: &[[[T; MR]; KC]],   // KC x MC (lda), packed-transposed, cache l2
@@ -84,6 +120,7 @@ where
         ldb: usize,            // ldb, for load and pack B
         ldc: usize,            // ldc, for write to C
         barrier: &[Mutex<()>], // barrier for writing to C
+        transb: bool,
     ) {
         // NR -> NC
         unsafe {
@@ -95,32 +132,31 @@ where
         let NR = NR_LANE * LANE;
         let ntask_mr = MC.div_ceil(MR);
         let mut buf_b: [[TySimd<T, LANE>; NR_LANE]; KC] = unsafe { zeroed() }; // KC x NR, packed, aligned, cache l1
+        let mut buf_trans_b: [[[T; LANE]; NR_LANE]; KC] = unsafe { zeroed() }; // KC x NR, temporary buffer for transpose
+        let buf_trans_b = unsafe { core::slice::from_raw_parts_mut(buf_trans_b.as_mut_ptr() as *mut T, KC * NR_LANE * LANE) };
 
         for (task_j, j) in (0..nc).step_by(NR).enumerate() {
             let nr = if j + NR <= nc { NR } else { nc - j };
-            if nr == NR_LANE * LANE {
-                for p in 0..kc {
-                    let b_ptr = unsafe { b.as_ptr().add(p * ldb + j) };
-                    for j_lane in 0..NR_LANE {
-                        buf_b[p][j_lane] = unsafe { TySimd::loadu_ptr(b_ptr.add(j_lane * LANE)) };
-                    }
-                }
-            } else {
-                // avoid out-of-bound access
-                for p in 0..kc {
-                    for jj in 0..nr {
-                        let j_lane = jj / LANE;
-                        let j_offset = jj % LANE;
-                        buf_b[p][j_lane][j_offset] = b[p * ldb + j + jj].clone();
-                    }
-                }
+            match transb {
+                true => Self::pack_b_trans(&mut buf_b, &b[j * ldb..], kc, nr, ldb, buf_trans_b),
+                false => Self::pack_b_no_trans(&mut buf_b, &b[j..], kc, nr, ldb),
             }
             unsafe { Self::matmul_loop_1st_mr(&mut c[j..], a, &buf_b, mc, nr, kc, ldc, &barrier[task_j * ntask_mr..]) };
         }
     }
 
-    pub fn matmul_loop_parallel_mnk_pack_a(c: &mut [T], a: &[T], b: &[T], m: usize, n: usize, k: usize, lda: usize, ldb: usize, ldc: usize)
-    where
+    pub fn matmul_loop_parallel_mnk_pack_a(
+        c: &mut [T],
+        a: &[T],
+        b: &[T],
+        m: usize,
+        n: usize,
+        k: usize,
+        lda: usize,
+        ldb: usize,
+        ldc: usize,
+        transb: bool,
+    ) where
         T: Send + Sync,
     {
         // compute number of tasks to split
@@ -171,7 +207,10 @@ where
             let task_k = task_id / (ntask_mc * ntask_nc);
 
             // get slices
-            let b = &b[task_k * KC * ldb + task_n * NC..];
+            let b = match transb {
+                true => &b[task_n * NC * ldb + task_k * KC..],
+                false => &b[task_k * KC * ldb + task_n * NC..],
+            };
             let mc = if (task_m + 1) * MC <= m { MC } else { m - task_m * MC };
             let nc = if (task_n + 1) * NC <= n { NC } else { n - task_n * NC };
             let kc = if (task_k + 1) * KC <= k { KC } else { k - task_k * KC };
@@ -180,18 +219,28 @@ where
             let barrier = &c_barrier[(task_m * ntask_nc + task_n) * (ntask_nr * ntask_mr)..];
             let c_mc_nc = unsafe { cast_mut_slice(&c[task_m * MC * ldc + task_n * NC..]) };
 
-            Self::matmul_loop_2nd_nr_pack_b(c_mc_nc, a_pack_mc_kc, b, mc, nc, kc, ldb, ldc, barrier);
+            Self::matmul_loop_2nd_nr_pack_b(c_mc_nc, a_pack_mc_kc, b, mc, nc, kc, ldb, ldc, barrier, transb);
         });
     }
 
-    pub fn matmul_loop_macro_mb(c: &mut [T], a: &[T], b: &[T], m: usize, n: usize, k: usize, lda: usize, ldb: usize, ldc: usize)
-    where
+    pub fn matmul_loop_macro_mb(
+        c: &mut [T],
+        a: &[T],
+        b: &[T],
+        m: usize,
+        n: usize,
+        k: usize,
+        lda: usize,
+        ldb: usize,
+        ldc: usize,
+        transb: bool,
+    ) where
         T: Send + Sync,
     {
         let mb_size = if MB == 0 { m } else { MB };
         for i in (0..m).step_by(mb_size) {
             let mb = if i + mb_size <= m { mb_size } else { m - i };
-            Self::matmul_loop_parallel_mnk_pack_a(&mut c[i * ldc..], &a[i * lda..], b, mb, n, k, lda, ldb, ldc);
+            Self::matmul_loop_parallel_mnk_pack_a(&mut c[i * ldc..], &a[i * lda..], b, mb, n, k, lda, ldb, ldc, transb);
         }
     }
 }
@@ -206,8 +255,9 @@ pub fn matmul_anyway_full(
     lda: usize,
     ldb: usize,
     ldc: usize,
+    transb: bool,
 ) {
-    MatmulLoops::<f64, 234, 512, 240, 13, 2, 8, 3744>::matmul_loop_macro_mb(c, a, b, m, n, k, lda, ldb, ldc);
+    MatmulLoops::<f64, 234, 512, 240, 13, 2, 8, 3744>::matmul_loop_macro_mb(c, a, b, m, n, k, lda, ldb, ldc, transb);
 }
 
 #[test]
@@ -223,7 +273,7 @@ fn test_matmul_anyway_full() {
 
     let time = std::time::Instant::now();
     let mut c: Vec<f64> = vec![0.0; m * ldc];
-    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc);
+    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc, false);
     let elapsed = time.elapsed();
     println!("Elapsed time: {:.3?}", elapsed);
 
@@ -233,6 +283,40 @@ fn test_matmul_anyway_full() {
     let b_tsr = rt::asarray((&b, [k, n], &device));
     let time = std::time::Instant::now();
     let c_ref = a_tsr % b_tsr;
+    let elapsed = time.elapsed();
+    println!("Elapsed time (ref): {:.3?}", elapsed);
+
+    let c_tsr = rt::asarray((&c, [m, n], &device));
+    let diff = &c_tsr - &c_ref;
+    println!("Max error: {:.6e}", diff.view().abs().max());
+
+    // println!("c_tsr\n{c_tsr:15.3}");
+    // println!("c_ref\n{c_ref:15.3}");
+}
+
+#[test]
+fn test_matmul_anyway_transb() {
+    let m = 3527;
+    let n = 9583;
+    let k = 6581;
+    let lda = k;
+    let ldb = k;
+    let ldc = n;
+    let a: Vec<f64> = (0..m * lda).into_par_iter().map(|x| (x as f64).sin()).collect();
+    let b: Vec<f64> = (0..n * ldb).into_par_iter().map(|x| (x as f64).cos()).collect();
+
+    let time = std::time::Instant::now();
+    let mut c: Vec<f64> = vec![0.0; m * ldc];
+    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc, true);
+    let elapsed = time.elapsed();
+    println!("Elapsed time: {:.3?}", elapsed);
+
+    use rstsr::prelude::*;
+    let device = DeviceOpenBLAS::default();
+    let a_tsr = rt::asarray((&a, [m, k], &device));
+    let b_tsr = rt::asarray((&b, [n, k], &device));
+    let time = std::time::Instant::now();
+    let c_ref = a_tsr % b_tsr.t();
     let elapsed = time.elapsed();
     println!("Elapsed time (ref): {:.3?}", elapsed);
 
@@ -262,19 +346,19 @@ fn test_matmul_faer_full() {
 
     let time = std::time::Instant::now();
     let mut c: Vec<f64> = vec![0.0; m * ldc];
-    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc);
+    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc, false);
     let elapsed = time.elapsed();
     println!("Elapsed time: {:.3?}", elapsed);
 
     let time = std::time::Instant::now();
     let mut c: Vec<f64> = vec![0.0; m * ldc];
-    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc);
+    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc, false);
     let elapsed = time.elapsed();
     println!("Elapsed time: {:.3?}", elapsed);
 
     let time = std::time::Instant::now();
     let mut c: Vec<f64> = vec![0.0; m * ldc];
-    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc);
+    matmul_anyway_full(&mut c, &a, &b, m, n, k, lda, ldb, ldc, false);
     let elapsed = time.elapsed();
     println!("Elapsed time: {:.3?}", elapsed);
 
